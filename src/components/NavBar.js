bee5db38ca37e82d9103b037497dbf38ed6c8c7d
rijan-1 <rijanrai125@gmail.com>
2023-12-19 10:44:46 +0000

    new change

---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import './NavBar.css'
+export const Navbar=()=>{
+
+    return(
+
+       
+
+            <bar className='navbar'>
+
+                <button>
+                    Hourly
+
+                </button>
+
+                <button>
+                    Daily
+
+                </button>
+                <button>
+                    Weekly
+
+                </button>
+
+                <button>
+                    Radar
+                    
+                </button>
+                <button>
+                Air quality
+
+                </button>
+
+                <button>
+                    Health advice
+                    
+                </button>
+
+            </bar>
+       
+    )
+}
